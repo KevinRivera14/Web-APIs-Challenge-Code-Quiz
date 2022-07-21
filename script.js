c011cf7733c832e2a5037a9f6b0647d39f9e11e9
Kevin Rivera <kevthebarber14@gmail.com>
2022-07-20 17:19:47 -0700

    new version of my project

--- script.js
+++ script.js
@@ -1,109 +1,121 @@
-const startButton = document.getElementById('Start-btn')
-const nextButton = document.getElementById('next-btn')
-const questionContainerElement = document.getElementById('question-container')
-const questionElement = document.getElementById('question')
-const answerbuttonsElement = document.getElementById('answer-buttons')
+// getting all required elements 
+const start_btn = document.querySelector(".start_btn  button"); 
+const info_box = document.querySelector(".info_box"); 
+const exit_btn = info_box.querySelector(".buttons .quit"); 
+const continue_btn = info_box.querySelector(".buttons .restart"); 
+const quiz_box = document.querySelector(".quiz_box"); 
+const timeCount = quiz_box.querySelector(".timer .timer_sec");
 
+const option_list = document.querySelector(".option_list"); 
 
-var shuffledQuestions, currentQuestionIndex
-
-startButton.addEventListener('click', startGame)
-
-
-function startGame() {
-    console.log('started')
-    startButton.classList.add('hide')
-    shuffledQuestions = question.sort(() => Math.random() - .5)
-    currentQuestionIndex = 0
-    questionContainerElement.classList.remove('hide')
-    setNextQestion()
-
+// if start quiz button clicked // 
+start_btn.oneclick = ()=>{
+    info_box.classList.add("activeInfo");
 }
 
-function setNextQestion() {
-    resetState()
-    showQuestion(shuffledQuestions[currentQuestionIndex])
+// if exit button is clicked // 
+exit_btn.oneclick = ()=>{
+    info_box.classList.remove("activeInfo");
 }
 
-function showQuestion(question) {
-    questionElement.innertext = question.question
-    question.answer.forEach(answer => {
-        const button = document.createElement('button')
-        button.innerText = answer.text
-        button.classList.add('btn')
-        if (answer.correct) {
-            button.dataset.correct = answer.correct
-        }
-        button.addEventListener('click', selectAnswer)
-        answerbuttonsElement.appendChild(button)
-    })
+// if continue button is clicked // 
+continue_btn.oneclick = ()=>{
+    info_box.classList.remove("activeInfo"); // hide the info box
+    quiz_box.classList.add("activequiz"); // hide the quiz box 
+    showQuestions(3); 
+    queCounter(1); 
+    startTimer(10); 
+}
+let que_count = 0;
+let que_numb = 1;
+let counter; 
+let timeValue =15; 
+
+const next_btn = quiz_box.querySelector(".next_btn");
+
+// if next button clicked // 
+next_btn.onclick = ()=>{
+    if(que_count < questions.length - 1){
+        que_count++;
+        que_numb++; 
+        showQuestions(que_count);
+        queCounter(que_numb);
+        clearInterval(timeValue);
+        startTimer(timeValue);
+    } else{ 
+        console.log("questions completed");
+    }
 }
 
 
-function resetState() {
-    nextButton.classlist.add('hide')
-    while (answerbuttonsElement.firstChild) {
-        answerbuttonsElement.removeChild(answerbuttonsElement.firstChild)
+
+
+//getting questions and options from array //
+
+function showQuestions(index) {
+    const que_text = document.querySelector(".que_text");
+    let   que_tag = '<span>'+ questions[index].numb + ". " + questions[index].question +'</span>';
+    let  option_tag = '<div class ="option">'+ questions[index].options[0] + '</span></span></div>'
+                        + '<div class="option">' + questions[index].options[1] + '<span></span></div>'
+                        + '<div class="option">' + questions[index].options[2] + '<span></span></div>'
+                        + '<div class="option">' + questions[index].options[3] + '<span></span></div>';
+
+    que_text.innerHTML = que_tag; 
+    option_list.innerHTML = que_tag;
+    const option = option_list.querySelectorAll(".option");
+    for (let i = 0; i < option.length; i++) {
+        option[i].setAttribute("onclick", "optionSelected(this)");
+    }  
+}
+
+let tickIcon = '<div class="icon tick ><i class="fas fa-check"></i></div>'; 
+let crossIcon = '<div class="icon cross"><i class="fas fa-times"></i></div>';
+
+
+
+function optionSelected(answer){
+    clearInterval(timeValue);
+    startTimer(timeValue); 
+    let userAns = answer.textContent;
+    let CorrectAns = questions[que_count].answer;
+    let allOptions = option_list.children.length;
+    if(userAns == CorrectAns){
+        answer.classList.add("correct");
+        console.log("answer is correct"); 
+        answer.insertAdjacentHTML("beforeend", tickIcon);
+    } else {
+        answer.classList.add("incorrect");
+        console.log("answer is wrong");
+        answer.insertAdjacentHTML("beforeend", crossIcon);
     }
 
-} 
+    // select answer automatically 
+    for (let i = 0; i < allOptions; i++) {
+        if (option_list.children[i].textContent == CorrectAns){
+            option_list.children[i].setAttribute("class", "option correct");
+            option_list.children[i].insertAdjacentHTML("beforeend", tickIcon); 
+        }
+    }    
+    
+}
 
-function selectAnswer(e) {
+function queCounter(index){
+    const bottom_ques_counter = quiz.box.querySelector(".total_que");
+    let totatQuesCountTag = '<span><p> ' + index + '</p>of<p>' + questions.length + '</p>Questions</span>';
+    bottom_ques_counter.innerHTML = totalQuesCountTag;
 
 }
 
-const question = [{
-    question: 'what is 2 + 2?',
-    answers: [{
-            text: '4',
-            correct: true
-        },
-        {
-            text: '22',
-            correct: false
-        }
-    ]
+// once user selected disabled all options 
 
-}]
+for ( let i = 0; i < allOptions; i++) {
+    option_list.children[i].classList.add('disabled');
+}
 
-
-
-//const question = {
-//question: "what is a nav bar?",
-// options: ["A = nav bar is a user interface element within a webpage that contains links","B = its part of the internet","C = its part of a document","D = nothing",] , 
-// correct: ["A"] 
-//};
-//GIVEN I am taking a code quiz
-
-// step 1: 
-// select the html start button  by using an one of the document function 
-
-//var startButton = document.querySelector(".start-button");
-
-
-
-// step 1 (A): 
-//WHEN I click the start button
-//THEN a timer starts and I am presented with a question  
-
-//var startQuiz = function () {
-//console.log("welcome to the quiz");
-//}
-//startButton.addEventListener("click", startQuiz);
-
-
-//step 2:
-//WHEN I answer a question
-//THEN I am presented with another question
-
-//step 3:
-//WHEN I answer a question incorrectly
-//THEN time is subtracted from the clock
-
-//step 4:
-//WHEN all questions are answered or the timer reaches 0
-//THEN the game is over
-
-// step 5: 
-//WHEN the game is over
-//THEN I can save my initials and score 
+function startTimer(time){
+    counter = setInterval(timer, 1000);
+    function timer(){
+        timeCount.textContent = time;
+        
+    }
+}
